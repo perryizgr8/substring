8c14afb92359164e8388472a73e0ce8a10b243f5
Parikshit Misra <perryizgr8@live.in>
2016-01-11 17:55:40 +0530

    Modified comparison logic to allow different lenghts.

--- Substring/Substring/Substring.cpp
+++ Substring/Substring/Substring.cpp
@@ -1,11 +1,79 @@
 // Substring.cpp : Defines the entry point for the console application.
-//
+// Enter 1, abcda, 1, dba. Output should be 3.
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
+int numOccur(string, string);
+bool compareFreq(string, string);
 
 int main()
 {
-    return 0;
+	int result = 0;
+	int testCases = 0;
+	int numQueries = 0;
+	string text;
+	string query;
+	cin >> testCases;
+	for (int i = 0; i < testCases; i++)
+	{
+		cin >> text;
+		cin >> numQueries;
+		for (int j = 0; j < numQueries; j++)
+		{
+			cin >> query;
+			result = numOccur(text, query);
+			cout << result << endl;
+		}
+	}
+	return 0;
 }
 
+int numOccur(string text, string pattern)
+{
+	//cout << "i was called! text:" << text << " patt:" << pattern << endl;
+	int res = 0, start = 0, end = 0;
+	int patLength = pattern.length();
+	int textLength = text.length();
+	for (int i = 0; i <= (textLength - patLength); i++)
+	{
+		//cout << i <<endl;
+		start = i;
+		end = i + (patLength - 1);
+		if (compareFreq(pattern, text.substr(start, end)) == true)
+		{
+			//cout << "found";
+			res++;
+			for (int j = i + (patLength - 1); j <= textLength - (i + 1); j++)
+			{
+				res++;
+			}
+		}
+	}
+	return res;
+}
+
+bool compareFreq(string one, string two)
+{
+	char count1[256] = { 0 }, count2[256] = { 0 };
+	int length1 = one.length();
+	int length2 = two.length();
+	for (int i = 0; i < length1; i++)
+	{
+		(count1[one[i]])++;
+	}
+	for (int i = 0; i < length2; i++)
+	{
+		(count2[two[i]])++;
+	}
+	for (int i = 0; i < 256; i++)
+	{
+		if (count1[i] <= count2[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
